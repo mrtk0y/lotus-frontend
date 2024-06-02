1e0c1fa99707e2c02159ac4849fe436291fabb86
ThinhBg Mark <thinhbg2812@gmail.com>
2024-06-03 01:19:45 +0700

    fix: remove env
    Signed-off-by: ThinhBg Mark <thinhbg2812@gmail.com>

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,3 +1,4 @@
-export const ACCESS_TOKEN = import.meta.env.VITE_ACCESS_TOKEN;
-export const API_URL = import.meta.env.VITE_BASE_URL;
-export const IMAGE_URL = import.meta.env.VITE_BASE_IMAGE_URL;
+export const ACCESS_TOKEN =
+	"eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkYTg3ZTcyZDE2YzllODYyZmIxY2VhOTE1NjVlZjE4MiIsInN1YiI6IjY2NWIxMzc2MjMxNjU0OGFlYTk4ZmU0YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.8Bb7prrDUvyJrQ-9zytR2rkVlKAE4nJUiS7e7quyFdA";
+export const API_URL = "https://api.themoviedb.org/3";
+export const IMAGE_URL = "https://image.tmdb.org/t/p/w500";
